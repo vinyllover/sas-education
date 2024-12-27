37b9ef88443ae55a0b16fef2350852493652547a
vinyllover <christopher.g.chan@gmail.com>
2024-12-27 16:19:31 +0000

    Added comment to mygit

--- mygit.bat
+++ mygit.bat
@@ -1,3 +1,5 @@
+REM setup instructions:  1.  submit config commands 2.  always add note before commiting 3. activate device
+
 git config --global user.email "christopher.g.chan@gmail.com"
 git config --global user.name "vinyllover"
 git config --list
